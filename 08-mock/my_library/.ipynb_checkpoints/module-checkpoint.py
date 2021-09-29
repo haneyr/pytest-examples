b3a52d44887c0d24402d15c582430c502326847b
Ryan Haney <haneyr@google.com>
2021-09-29 14:42:58 +0000

    fixing .gitignore

--- 08-mock/my_library/.ipynb_checkpoints/module-checkpoint.py
+++
@@ -1,7 +0,0 @@
-def is_dataframe_big(dataframe):
-
-    # Lets check if this dataframe has over a million rows
-    if dataframe.shape[0] > 1000000:
-        return True
-    else:
-        return False
